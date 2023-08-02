2b6c954578c3fc3e5489e351e1127b6bd5c63a01
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-08-02 17:30:27 +0530

    Update 01 0_1 Knapsack.cpp

--- Dynamic Programming/01 0_1 Knapsack.cpp
+++ Dynamic Programming/01 0_1 Knapsack.cpp
@@ -3,12 +3,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int knapsack(int wt[], int val[], int W, int n){
+int knapsack(int wt[], int val[],int W, int n){
     int dp[n+1][W+1];
     
     for(int i=0;i<n+1;i++){
         for(int j=0;j<W+1;j++){
-            if(i==0 || j==0){
+            if(i==0 ||j==0){
                 dp[i][j] = 0;
             }
         }
@@ -17,7 +17,7 @@ int knapsack(int wt[], int val[], int W, int n){
     for(int i=1;i<n+1;i++){
         for(int j=1;j<W+1;j++){
             if(wt[i-1]<=j){
-                dp[i][j] = max(val[i-1]+dp[n-1][j-wt[i-1]], dp[i-1][j]);
+                dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]], dp[i-1][j]);
             }
             else if(wt[i-1]>j){
                 dp[i][j] = dp[i-1][j];
