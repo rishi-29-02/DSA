f2e786b995e1ca4f8d8b8a00c92a8ae5b105ef66
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-09-30 11:43:48 +0530

    Create 01 Binary Search.cpp

---
+++ Searching/01 Binary Search.cpp
@@ -0,0 +1,35 @@
+// Binary Search 
+
+// Time Complexity :: O(log(n))
+// Space Complexity :: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool binarySearch(int arr[], int st, int end, int k){
+    int mid = st + (end-st)/2;
+    
+    if(st<=end){
+        if(arr[mid]==k){
+            return true;
+        }
+        else if(arr[mid]<k){
+            return binarySearch(arr, mid+1, end, k);
+        }
+        else if(arr[mid]>k){
+            return binarySearch(arr, st, mid-1, k);
+        }    
+    }
+    else{
+        return false;
+    }
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);ac
+    
+    int k=2;
+    cout << binarySearch(arr, 0, n-1, k);
+    return 0;
+}
