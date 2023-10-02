9265f078e044bcc2e80e51bcb7e4e0b9569fb22e
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-10-02 13:46:25 +0530

    Create 07 Buy and Sell Stock VI (with transsactions fee).cpp

---
+++ Top 100 DSA Interview Qs/07 Buy and Sell Stock VI (with transsactions fee).cpp
@@ -0,0 +1,23 @@
+// Buy and Sell Stock VI (with transsactions fee)
+
+int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        
+        vector<vector<int>> dp(n+1, vector<int> (2, 0));
+        
+        
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){
+                    dp[idx][buy] = max(-prices[idx] + dp[idx+1][0],
+                                            0 + dp[idx+1][1]);
+                }
+                else{
+                    dp[idx][buy] = max(prices[idx]-fee + dp[idx+1][1],
+                                            0 + dp[idx+1][0]);
+                }
+            }
+        }
+        
+        return dp[0][1];
+    }
