1b0d48b4594a6e8fef7c3a99d4f8218ad05b4544
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-10-02 11:46:17 +0530

    Update 04 Buy and Sell Stock III (At most two transactions are allowed).cpp

--- Top 100 DSA Interview Qs/04 Buy and Sell Stock III (At most two transactions are allowed).cpp
+++ Top 100 DSA Interview Qs/04 Buy and Sell Stock III (At most two transactions are allowed).cpp
@@ -47,6 +47,45 @@ int f(int idx, int buy, int cap, int prices[], int n,
     }
 }
 
+// top-down
+int f(int idx, int buy, int cap, int prices[], int n,
+            vector<vector<vector<int>>> &dp){
+    
+    // Initialization
+    // a. cap==0 
+    for(int idx=0;idx<=n;idx++){
+        for(int buy=0;buy<=1;buy++){
+            dp[idx][buy][0] = 0;
+        }
+    }
+    
+    // b. idx==n 
+    for(int buy=0;buy<=1;buy++){
+        for(int cap=0;cap<=2;cap++){
+            dp[n][buy][cap] = 0;
+        }
+    }
+    
+    for(int idx=n-1;idx>=0;idx--){
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=1;cap<=2;cap++){
+                // Two choices :: buy or skip
+                if(buy){
+                    dp[idx][buy][cap] = max(-prices[idx] + dp[idx+1][0][cap],
+                                    0 + dp[idx+1][1][cap]);
+                }
+                // Two choices :: sell or skip
+                else{
+                    dp[idx][buy][cap] = max(prices[idx] + dp[idx+1][1][cap-1],
+                                    0 + dp[idx+1][0][cap]);
+                }
+            }
+        }
+    }
+    
+    return dp[0][1][2];
+}
+
 int main(){
     int prices[] = {3, 3, 5, 0, 0, 3, 1, 4};
     int n = sizeof(prices)/sizeof(prices[0]);
@@ -57,7 +96,34 @@ int main(){
     // Memoization 
     vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (3, -1)));
     cout << f(0, 1, 2, prices, n, dp);
+    
+    // Top-down dp
+    vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, 0)));
+    cout << f(0, 1, 2, prices, n, dp);
 
+    // Optimized Top-down
+    vector<vector<int>> after(2, vector<int> (3, 0));
+    vector<vector<int>> curr(2, vector<int> (3, 0));
+    
+    for(int idx=n-1;idx>=0;idx--){
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=1;cap<=2;cap++){
+                // Two choices :: buy or skip
+                if(buy){
+                    curr[buy][cap] = max(-prices[idx] + after[0][cap],
+                                    0 + after[1][cap]);
+                }
+                // Two choices :: sell or skip
+                else{
+                    curr[buy][cap] = max(prices[idx] + after[1][cap-1],
+                                    0 + after[0][cap]);
+                }
+            }
+        }
+        after = curr;
+    }
+    
+    cout << curr[1][2];
     
     return 0;
 }
