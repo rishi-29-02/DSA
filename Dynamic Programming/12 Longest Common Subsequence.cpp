548dab91c1bb2e2fab2dbe98523cead41207deb2
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-10-31 07:30:20 +0530

    Update 12 Longest Common Subsequence.cpp

--- Dynamic Programming/12 Longest Common Subsequence.cpp
+++ Dynamic Programming/12 Longest Common Subsequence.cpp
@@ -74,7 +74,7 @@ int LCS(string x, string y, int n, int m){
     
     for(int i=1;i<n+1;i++){
         for(int j=1;j<m+1;j++){
-            if(x[i]==y[j]){
+            if(x[i-1]==y[j-1]){
                 dp[i][j] = 1+dp[i-1][j-1];
             }
             else{
