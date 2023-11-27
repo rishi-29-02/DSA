5e4507dab3e3a69ca01d7008dec1d7adb3b68f38
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-11-27 12:32:20 +0530

    Create Shortest Common Supersequence.cpp

---
+++ Dynamic Programming/Shortest Common Supersequence.cpp
@@ -0,0 +1,37 @@
+// Shortest Common Supersequence 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int shComSup(string x, string y, int n, int m){
+    int dp[n+1][m+1];
+    
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0 || j==0){
+                dp[i][j] = 0;
+            }
+        }
+    }
+    
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(x[i-1]==y[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    
+    return x.size() + y.size() - dp[n][m];
+}
+
+int main(){
+    string x = "geek";
+    string y = "eke";
+    
+    cout << shComSup(x, y, x.size(), y.size());
+    
+    return 0;
+}
