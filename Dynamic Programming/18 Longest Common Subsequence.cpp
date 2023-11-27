dc399a8ce7e9b7857264d545fa340179d8461638
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-11-27 16:09:15 +0530

    Create 18 Longest Common Subsequence.cpp

---
+++ Dynamic Programming/18 Longest Common Subsequence.cpp
@@ -0,0 +1,38 @@
+// Print the Longest Common Palindromic Subsequence 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int LPS(string x, string y, int n, int m){
+    int dp[n+1][m+1];
+    
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0 || j==0){
+                dp[i][j] = 0;
+            }
+        }
+    }
+    
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(x[i-1]==y[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+int main(){
+    string x = "bbbab";
+    string a = x;
+    reverse(x.begin(), x.end());
+    string b = x;
+    
+    cout << LPS(a, b, a.size(), b.size());
+    
+    return 0;
+}
