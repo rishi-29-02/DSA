e85e734d2a780518376aad654ddf262ab21e223e
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-12-27 11:57:47 +0530

    Update 01 First negative integer in each subarray of size k.cpp

--- Sliding Window/01 First negative integer in each subarray of size k.cpp
+++ Sliding Window/01 First negative integer in each subarray of size k.cpp
@@ -1,31 +1,40 @@
-// Find the first negative integer in each subarray of size k
+// First negative number in each subarray of size k 
 
-vector<long long> printFirstNegativeInteger(long long int arr[],
-                                             long long int n, long long int k) {
-                                                 long long int i=0, j=0;
-                                                 vector<long long> ans;
-                                                 deque<long long> nlist;
-                                                 
-                                                 while(j<n){
-                                                     if(arr[j]<0){
-                                                         nlist.push_back(arr[j]);
-                                                     }
-                                                     
-                                                     if(j-i+1<k){
-                                                         j++;
-                                                     }
-                                                     else if(j-i+1==k){
-                                                         if(nlist.size()==0){
-                                                             ans.push_back(0);
-                                                         }
-                                                         else{
-                                                             ans.push_back(nlist.front());
-                                                             if(arr[i]==nlist.front()){
-                                                                 nlist.pop_front();
-                                                             }
-                                                         }
-                                                         i++;j++;
-                                                     }
-                                                 }
-                                                 return ans;
- }
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> firstNegative(int arr[], int n, int k){
+    vector<int> v;
+    int i=0, j=0;
+    queue<int> q;
+    while(j<n){
+        if(arr[j]<0) q.push(arr[j]);
+        
+        if((j-i+1)==k){
+            if(q.empty()){
+                v.push_back(0);
+            } else {
+                v.push_back(q.front());
+                if(arr[i]==q.front()){
+                    q.pop();
+                }
+            }
+            i++;j++;
+        } else {
+            j++;
+        }
+    }
+    return v;
+}
+
+int main(){
+    int arr[] = {4, -3, 1, 6, -2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k=2;
+    
+    vector<int> v = firstNegative(arr, n, k);
+    for(auto it:v){
+        cout << it << " ";
+    }
+    return 0;
+}
