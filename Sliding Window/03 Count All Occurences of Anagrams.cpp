3be6d18a75180e4903d72a25798a19127b7570b3
RISHI MISHRA <67303857+rishi-29-02@users.noreply.github.com>
2023-12-27 15:33:08 +0530

    Update 03 Count All Occurrences of Anagrams.cpp

--- Sliding Window/03 Count All Occurences of Anagrams.cpp
+++ Sliding Window/03 Count All Occurences of Anagrams.cpp
@@ -1,45 +1,49 @@
-// Count All Occurences of Anagrams
+// Count all the anagrams in the given string 
 
-int search(string pat, string s) {
-	    // code here
-	    int n = s.size();
-	    int k=pat.size();
-	    
-	    int i=0,j=0;
-	    
-	    unordered_map<char, int> mp;
-	    for(int i=0;i<k;i++){
-	        mp[pat[i]]++;
-	    }
-	    
-	    int cnt = mp.size();
-	    int ans=0;
-	    
-	    while(j<n){
-	        if(mp.find(s[j])!=mp.end()){
-	            mp[s[j]]--;
-	            if(mp[s[j]]==0){
-	                cnt--;
-	            }
-	        }
-	        
-	        if(j-i+1<k){
-	            j++;
-	        }
-	        else if(j-i+1==k){
-	            if(cnt==0){
-	                ans++;
-	            }
-	            
-	            if(mp.find(s[i])!=mp.end()){
-	                mp[s[i]]++;
-	                if(mp[s[i]]==1){
-	                    cnt++;
-	                }
-	            }
-	            i++;j++;
-	        }
-	    }
-	    
-	    return ans;
-	}
+#include <bits/stdc++.h>
+using namespace std;
+
+int allAnagrams(string s, string sb){
+    unordered_map<char, int> mp;
+    for(auto it:sb){
+        mp[it]++;
+    }
+    
+    int ans=0;
+    int i=0, j=0;
+    int n = s.size();
+    int k = sb.size();
+    int cnt = mp.size();
+    
+    while(j<n){
+        mp[s[j]]--;
+        if(mp[s[j]]==0){
+            mp.erase(s[j]);
+            cnt--;
+        }
+        
+        if(j-i+1==k){
+            if(cnt==0){
+                ans++;  
+            }
+            if(mp.find(s[i])!=mp.end()){
+                mp[s[i]]++;
+            } else {
+                mp[s[i]] =1;
+                cnt++;
+            }
+            i++;j++;
+        } else {
+            j++;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    string s = "aabaabaa";
+    string sb = "aaba";
+    
+    cout << allAnagrams(s, sb); // 4
+    return 0;
+}
